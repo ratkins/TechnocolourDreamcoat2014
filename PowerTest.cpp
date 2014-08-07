a789f14c783af4a310005d300857678290ce05cf
Robert Atkins <ratkins@fastmail.fm>
2014-08-07 22:27:42 +0200

    Adding power-test code to help troubleshoot for Mark K

---
+++ PowerTest.cpp
@@ -0,0 +1,24 @@
+#ifndef PowerTest_h
+#define PowerTest_h
+
+#include "FastLED.h"
+#include "Effect.cpp"
+
+class PowerTest : public Effect {
+  
+  private:
+    CRGB colour;
+  
+  public:
+    PowerTest(CRGB *leds, CRGB colour) : Effect(leds), colour(colour) {}
+    
+    virtual void draw(int rawPot, int rawMic, bool button) {
+        int ledsIdx = 0;
+        for (int i = 0; i < 100; i++) {
+            leds[(ledsIdx += 37) % NUM_LEDS] = colour;
+        }
+    }
+
+};
+
+#endif
